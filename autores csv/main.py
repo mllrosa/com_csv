d54334f8309f7751156dbe7d47a5bdb347346d8d
mllrosa <mllr8sa@gmail.com>
2025-08-26 14:06:58 -0300

    salvei a atividade de buscar autores(autorescsv)

---
+++ autores csv/main.py
@@ -0,0 +1,13 @@
+import csv
+
+with open("dados.csv",newline="",encoding="utf-8") as arquivo:
+    arquivo_csv = csv.DictReader(arquivo)
+
+    #dados = list(arquivo_csv)
+    
+    for linha in arquivo_csv:
+        autores = linha["Author(s)"]
+        
+        if linha ["Author(s)"] == "Fortunato, Victoria Della Ventura, Hernández, Nahuel Nicolás, Vázquez, Damián Ramírez":
+            linha["Author(s)"].split(',')
+            print(linha["Author(s)"])
